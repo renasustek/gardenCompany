2adf92cea75983ae19287e80bef2d36c5fcfe9e5
renas <renasustek@gmail.com>
2018-09-02 21:44:57 +0100

    idc

--- gardenCompanyProgram.py
+++ gardenCompanyProgram.py
@@ -1,8 +1,9 @@
 def startMenu():
-    menuOption = input("""Press 1: Create Garden"
+    menuOption = input("""Press 1: Create Garden
 Press 2: Reviews
 Press 3: Old documents
-Press 4: Exit""")
+Press 4: Exit
+>>""")
 
     if menuOption == "1":
         createGarden()
@@ -21,9 +22,29 @@ def createGarden():
         totalSizeOfLawn = lengthOfLawn*widthOfLawn
         totalPriceOfLawn = totalSizeOfLawn * 15.50
         totalTimeOfLawn = totalSizeOfLawn * 20
-        print("Price:",totalPriceOfLawn+"m^2, Time Taken:",totalTimeOfLawn)
+        print("Price: £",totalPriceOfLawn,"m^2, Time to Take:",totalTimeOfLawn,"minutes")
     elif wantLawn == "n":
-        pass
+        print("No lawn for you")
+    wantConcretePatio = input("Do you want a Concrete Patio: y/n\n>>").lower()
+    if wantConcretePatio == "y":
+        lengthOfConcretePatio = float(input("Enter length of Concrete Patio:\n>> "))
+        widthOfConcretePatio = float(input("Enter width of Concrete Patio:\n>> "))
+        totalSizeOfConcretePatio = lengthOfConcretePatio * widthOfConcretePatio
+        totalPriceOfConcretePatio = totalSizeOfConcretePatio * 20.99
+        totalTimeOfConcretePatio = totalSizeOfConcretePatio * 20
+        print("Price: £", totalPriceOfConcretePatio, "m^2, Time to Take:", totalTimeOfConcretePatio, "minutes")
+    elif wantConcretePatio == "n":
+        print("No Concrete Patio for you")
+    wantWoodenDeck = input("Do you want a lawn: y/n\n>>").lower()
+    if wantWoodenDeck == "y":
+        lengthOfWoodenDeck = float(input("Enter length of WoodenDeck:\\n>> "))
+        widthOfWoodenDeck = float(input("Enter width of Wooden Deck:\n>> "))
+        totalSizeOfWoodenDeck = lengthOfWoodenDeck * widthOfLawn
+        totalPriceOfWoodenDeck = totalSizeOfWoodenDeck * 15.50
+        totalTimeOfWoodenDeck = totalSizeOfWoodenDeck * 20
+        print("Price: £", totalPriceOfWoodenDeck, "m^2, Time to Take:", totalTimeOfWoodenDeck, "minutes")
+    elif wantWoodenDeck== "n":
+        print("No WoodenDeck for you")
 def reviews():
     pass
 
