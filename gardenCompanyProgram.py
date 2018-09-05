876ffec738fab8088e9426767ce04feb8a104aa5
renas <renasustek@gmail.com>
2018-09-05 21:31:58 +0100

    not working, going to get it to work later

--- gardenCompanyProgram.py
+++ gardenCompanyProgram.py
@@ -1,16 +1,16 @@
 def startMenu():
     menuOption = input("""Press 1: Create Garden
 Press 2: Reviews
-Press 3: Old documents
+Press 3: documents
 Press 4: Exit
 >>""")
 
     if menuOption == "1":
-        wantLawn()
+        name()
     elif menuOption == "2":
         reviews()
     elif menuOption == "3":
-        Documnents()
+        documents()
     elif menuOption == "4":
         exit()
     else:
@@ -18,15 +18,20 @@ Press 4: Exit
         startMenu()
 
 
-def Documnents():
+def writeToDocumnents():
     garden = open("garden.txt", "a")
-    garden.write(tuple(theLawn))
-    garden.close()
+    garden.write(name+":\nLawn:\n"+theLawn+"\nConcrete patio:\n"+theConcretePatio+"\nWooden Deck:\n"+theWoodDeck\
++"\nPond:\n"+thePond+"\nthe water feature:\n"+theWater+"\nGarden lights:\n"+theLawn+"\nTotal cost for materials:\n"\
++totalCostForMats+"\nTotal cost for labour:\n"+totalCostForLabour+"\nTotal cost :\n"+totalCost)
     startMenu()
 
+def name():
+    global name
+    name = input("What is your name:\n>>")
+    wantLawn()
 
 def wantLawn():
-    global theLawn
+    global theLawn,totalPriceOfLawn
     wantLawnAnswer = input("Do you want a lawn: y/n\n>>").lower()
     if wantLawnAnswer == "y":
         lengthOfLawn = float(input("Enter length of lawn:\n>> "))
@@ -35,7 +40,8 @@ def wantLawn():
         totalPriceOfLawn = totalSizeOfLawn * 15.50
         totalTimeOfLawn = totalSizeOfLawn * 20
         print("Price: £", totalPriceOfLawn, "m^2, Time to Take:", totalTimeOfLawn, "minutes")
-        theLawn = totalPriceOfLawn,totalTimeOfLawn,totalSizeOfLawn
+        theLawn = "£"+str(totalPriceOfLawn)+""+str(totalTimeOfLawn)+" minutes " \
++ str(totalSizeOfLawn )+"m^2 ("+str(lengthOfLawn)+"m"+str(widthOfLawn)+"m)"
     elif wantLawnAnswer == "n":
         print("No lawn for you")
     else:
@@ -44,7 +50,7 @@ def wantLawn():
     wantConcretePatio()
 
 def wantConcretePatio():
-    global theConcretePatio
+    global theConcretePatio,totalPriceOfConcretePatio
     wantConcretePatioAnswer = input("Do you want a Concrete Patio: y/n\n>>").lower()
     if wantConcretePatioAnswer == "y":
         lengthOfConcretePatio = float(input("Enter length of Concrete Patio:\n>> "))
@@ -53,7 +59,8 @@ def wantConcretePatio():
         totalPriceOfConcretePatio = totalSizeOfConcretePatio * 20.99
         totalTimeOfConcretePatio = totalSizeOfConcretePatio * 20
         print("Price: £", totalPriceOfConcretePatio, "m^2, Time to Take:", totalTimeOfConcretePatio, "minutes")
-        theConcretePatio = totalPriceOfConcretePatio,totalTimeOfConcretePatio,totalSizeOfConcretePatio
+        theConcretePatio = "£"+str(totalPriceOfConcretePatio)+""+str(totalTimeOfConcretePatio)+" minutes " + str(totalSizeOfConcretePatio)+\
+"m^2 ("+str(lengthOfConcretePatio)+"m"+str(widthOfConcretePatio)+"m)"
     elif wantConcretePatioAnswer == "n":
         print("No Concrete Patio for you")
     else:
@@ -63,6 +70,7 @@ def wantConcretePatio():
 
 
 def wantWoodenDeck():
+    global theWoodDeck,totalPriceOfWoodenDeck
     wantWoodenDeckAnswer = input("Do you want a woodendeck: y/n\n>>").lower()
     if wantWoodenDeckAnswer == "y":
         lengthOfWoodenDeck = float(input("Enter length of WoodenDeck:\n>> "))
@@ -71,6 +79,8 @@ def wantWoodenDeck():
         totalPriceOfWoodenDeck = totalSizeOfWoodenDeck * 15.75
         totalTimeOfWoodenDeck = totalSizeOfWoodenDeck * 30
         print("Price: £", totalPriceOfWoodenDeck, "m^2, Time to Take:", totalTimeOfWoodenDeck, "minutes")
+        theWoodDeck = "£" + str(totalPriceOfWoodenDeck) + "" + str(totalTimeOfWoodenDeck) + " minutes " + str(totalSizeOfWoodenDeck) + \
+"m^2 (" + str(lengthOfWoodenDeck) + "m" + str(widthOfWoodenDeck) + "m)"
     elif wantWoodenDeckAnswer == "n":
         print("No WoodenDeck for you")
     else:
@@ -80,6 +90,7 @@ def wantWoodenDeck():
 
 
 def wantPond():
+    global thePond,totalPriceOfPond
     wantPondAnswer = input("Do you want a Pond: y/n\n>>").lower()
     if wantPondAnswer == "y":
         lengthOfPond = float(input("Enter length of WoodenDeck:\n>> "))
@@ -88,6 +99,8 @@ def wantPond():
         totalPriceOfPond = totalSizeOfPond * 25
         totalTimeOfPond = totalSizeOfPond * 45
         print("Price: £", totalPriceOfPond, "m^2, Time to Take:", totalTimeOfPond, "minutes")
+        thePond = "£" + str(totalPriceOfPond) + "" + str(totalTimeOfPond) + " minutes " + str(totalSizeOfPond) + \
+"m^2 (" + str(lengthOfPond) + "m" + str(widthOfPond) + "m)"
     elif wantPondAnswer == "n":
         print("No Pond for you")
     else:
@@ -96,12 +109,14 @@ def wantPond():
     wantWaterFeature()
 
 def wantWaterFeature():
+    global theWater,priceOfWaterFeature
     wantWaterFeatureAnswer = input("Want a water feature y/n:\n>>").lower()
     if wantWaterFeatureAnswer == "y":
         howManyWaterFeatures = float(input("How many water featres do you want:\n>>"))
         priceOfWaterFeature = howManyWaterFeatures * 150
         timeToTakeForWaterFeature = howManyWaterFeatures * 60
         print("Price: £", priceOfWaterFeature, "m^2, Time to Take:", timeToTakeForWaterFeature, "minutes")
+        theWater = "£" + str(priceOfWaterFeature) + "" + str(timeToTakeForWaterFeature) + " minutes " + str(howManyWaterFeatures)+"water features"
     elif wantWaterFeatureAnswer == "n":
         print("No water feature for you:")
     else:
@@ -110,19 +125,27 @@ def wantWaterFeature():
     wantLamp()
 
 def wantLamp():
+    global theLamp,priceOflamp
     wantlampAnswer = input("Want a water feature y/n:\n>>").lower()
     if wantlampAnswer == "y":
         howManylamp = float(input("How many water featres do you want:\n>>"))
         priceOflamp = howManylamp * 5
         timeToTakeForlamp = howManylamp * 10
         print("Price: £", priceOflamp, "m^2, Time to Take:", timeToTakeForlamp, "minutes")
+        thelamp = "£" + str(priceOflamp) + "" + str(timeToTakeForlamp) + " minutes " + str(howManylamp)+"lamps"
     elif wantlampAnswer == "n":
         print("No lamp for you:")
     else:
         print("Invalid Option, try again.")
         wantLamp()
 
-    Documnents()
+    writeToDocumnents()
+def totalCosts():
+    global totalCostForLabour,totalCostForMats,totalCost
+    totalCost=
+
+def documents():
+    pass
 
 def reviews():
     pass
